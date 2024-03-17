10a9045d9df601d22b680b8c4298dd5fa4864a21
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-18 03:38:19 +0500

    Users Data Displayed

--- lib/Widgets/row.dart
+++ lib/Widgets/row.dart
@@ -8,12 +8,15 @@ class RowWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(title),
-        Text(value),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title),
+          Text(value),
+        ],
+      ),
     );
   }
 }
