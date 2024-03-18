a6f6ecd6edbfa735fab6ba41f025f43cb7badbfc
Muhammad Usman <163582723+musman023@users.noreply.github.com>
2024-03-18 15:23:01 +0500

    fetched photos and displayed

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -2,11 +2,12 @@
 
 import 'dart:convert';
 
-import 'package:api/Models/posts_model.dart';
-import 'package:api/Models/user_model.dart';
-import 'package:api/Widgets/row.dart';
+// import 'package:api/Models/posts_model.dart';
+import 'package:api/Models/products_model.dart';
+// import 'package:api/Models/user_model.dart';
+// import 'package:api/Widgets/row.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+// import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
@@ -16,20 +17,31 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-List<UserModel> userList = [];
-Future<List<UserModel>> getUserApi() async {
-  final response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
-  var data = jsonDecode(response.body) as List<dynamic>;
+Future<ProductsModel> getProductsApi() async {
+  final response = await http.get(
+      Uri.parse('https://webhook.site/8df1e1b3-4aff-4df0-8739-d22657028c73'));
+  var data = jsonDecode(response.body.toString());
   if (response.statusCode == 200) {
-    for (Map<String, dynamic> i in data) {
-      userList.add(UserModel.fromJson(i));
-    }
-    return userList;
+    return ProductsModel.fromJson(data);
   } else {
-    return userList;
+    return ProductsModel.fromJson(data);
   }
 }
+
+// List<UserModel> userList = [];
+// Future<List<UserModel>> getUserApi() async {
+//   final response =
+//       await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
+//   var data = jsonDecode(response.body) as List<dynamic>;
+//   if (response.statusCode == 200) {
+//     for (Map<String, dynamic> i in data) {
+//       userList.add(UserModel.fromJson(i));
+//     }
+//     return userList;
+//   } else {
+//     return userList;
+//   }
+// }
 
 // List<PostsModel> postList = [];
 
@@ -54,80 +66,157 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text("Api Integration"),
       ),
-      body: Column(
-        children: [
-          Expanded(
-              child: FutureBuilder(
-                  future: getUserApi(),
-                  builder: (context, AsyncSnapshot<List<UserModel>> snapshot) {
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    } else {
-                      return ListView.builder(
-                        itemCount: userList.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Expanded(
+                child: FutureBuilder<ProductsModel>(
+                    future: getProductsApi(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.data!.length,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  RowWidget(
-                                      title: 'Name',
-                                      value: snapshot.data![index].name
+                                  ListTile(
+                                    title: Text(snapshot
+                                        .data!.data![index].shop!.name
+                                        .toString()),
+                                    subtitle: Text(snapshot
+                                        .data!.data![index].shop!.shopemail
+                                        .toString()),
+                                    leading: CircleAvatar(
+                                      backgroundImage: NetworkImage(snapshot
+                                          .data!.data![index].shop!.image
                                           .toString()),
-                                  RowWidget(
-                                      title: 'Username',
-                                      value: snapshot.data![index].username
-                                          .toString()),
-                                  RowWidget(
-                                      title: 'Email',
-                                      value: snapshot.data![index].email
-                                          .toString()),
-                                  RowWidget(
-                                      title: 'Phone',
-                                      value: snapshot.data![index].phone
-                                          .toString()),
-                                  RowWidget(
-                                      title: 'Company Name',
-                                      value: snapshot.data![index].company!.name
-                                          .toString()),
-                                  RowWidget(
-                                      title: 'Address',
-                                      value:
-                                          '${snapshot.data![index].address!.street}, ${snapshot.data![index].address!.city}'),
+                                    ),
+                                  ),
+                                  Container(
+                                    height:
+                                        MediaQuery.of(context).size.height * .3,
+                                    width:
+                                        MediaQuery.of(context).size.width * 1,
+                                    child: ListView.builder(
+                                        scrollDirection: Axis.horizontal,
+                                        itemCount: snapshot
+                                            .data!.data![index].images!.length,
+                                        itemBuilder: (context, position) {
+                                          return Padding(
+                                            padding: const EdgeInsets.only(
+                                                right: 10.0, bottom: 10),
+                                            child: Container(
+                                              height: MediaQuery.of(context)
+                                                      .size
+                                                      .height *
+                                                  .25,
+                                              width: MediaQuery.of(context)
+                                                      .size
+                                                      .width *
+                                                  .5,
+                                              decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                  image: DecorationImage(
+                                                      fit: BoxFit.cover,
+                                                      image: NetworkImage(
+                                                          snapshot
+                                                              .data!
+                                                              .data![index]
+                                                              .images![position]
+                                                              .url
+                                                              .toString()))),
+                                            ),
+                                          );
+                                        }),
+                                  ),
+                                  Icon(snapshot.data!.data![index].inWishlist ==
+                                          true
+                                      ? Icons.favorite
+                                      : Icons.favorite_outline)
                                 ],
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    }
-                  })
+                              );
+                            });
+                      }
+                    })
 
-              //  FutureBuilder(
-              //     future: getPostApi(),
-              //     builder: (context, snapshot) {
-              //       if (!snapshot.hasData) {
-              //         return Center(child: CircularProgressIndicator());
-              //       } else {
-              //         return ListView.builder(
-              //             itemCount: postList.length,
-              //             itemBuilder: (context, index) {
-              //               return Card(
-              //                   child: ListTile(
-              //                 leading: Text(
-              //                   postList[index].id.toString(),
-              //                   style: TextStyle(fontSize: 20),
-              //                 ),
-              //                 title: Text(postList[index].title.toString()),
-              //               ));
-              //             });
-              //       }
-              //     }),
-              )
-        ],
+                //  FutureBuilder(
+                //     future: getUserApi(),
+                //     builder: (context, AsyncSnapshot<List<UserModel>> snapshot) {
+                //       if (!snapshot.hasData) {
+                //         return Center(
+                //           child: CircularProgressIndicator(),
+                //         );
+                //       } else {
+                //         return ListView.builder(
+                //           itemCount: userList.length,
+                //           itemBuilder: (BuildContext context, int index) {
+                //             return Card(
+                //               child: Padding(
+                //                 padding: const EdgeInsets.all(8.0),
+                //                 child: Column(
+                //                   children: [
+                //                     RowWidget(
+                //                         title: 'Name',
+                //                         value: snapshot.data![index].name
+                //                             .toString()),
+                //                     RowWidget(
+                //                         title: 'Username',
+                //                         value: snapshot.data![index].username
+                //                             .toString()),
+                //                     RowWidget(
+                //                         title: 'Email',
+                //                         value: snapshot.data![index].email
+                //                             .toString()),
+                //                     RowWidget(
+                //                         title: 'Phone',
+                //                         value: snapshot.data![index].phone
+                //                             .toString()),
+                //                     RowWidget(
+                //                         title: 'Company Name',
+                //                         value: snapshot.data![index].company!.name
+                //                             .toString()),
+                //                     RowWidget(
+                //                         title: 'Address',
+                //                         value:
+                //                             '${snapshot.data![index].address!.street}, ${snapshot.data![index].address!.city}'),
+                //                   ],
+                //                 ),
+                //               ),
+                //             );
+                //           },
+                //         );
+                //       }
+                //     })
+
+                //  FutureBuilder(
+                //     future: getPostApi(),
+                //     builder: (context, snapshot) {
+                //       if (!snapshot.hasData) {
+                //         return Center(child: CircularProgressIndicator());
+                //       } else {
+                //         return ListView.builder(
+                //             itemCount: postList.length,
+                //             itemBuilder: (context, index) {
+                //               return Card(
+                //                   child: ListTile(
+                //                 leading: Text(
+                //                   postList[index].id.toString(),
+                //                   style: TextStyle(fontSize: 20),
+                //                 ),
+                //                 title: Text(postList[index].title.toString()),
+                //               ));
+                //             });
+                //       }
+                //     }),
+                )
+          ],
+        ),
       ),
     );
   }
